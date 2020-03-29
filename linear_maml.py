26906b2ad4f1de42925aeb4f9212f9cef160c0a7
Mehrdad Yazdani <mehrdad@january.ai>
2020-03-28 18:01:01 -0700

    removed the inner loop update to its own function

--- linear_maml.py
+++ linear_maml.py
@@ -18,27 +18,33 @@ class LinearMetaLearn:
             self.w = w
             
         self.mses = []
+        
+    def inner_loop_updates(self, X, y, num_epochs):
+        for _ in range(num_epochs):
+            y_pred = X @ self.w
+            delta = (y_pred - y)[:,np.newaxis]
+            self.w = self.w - self.alpha * np.sum(delta * X,0) 
+    
+    def predict(self, X):
+        return X @ self.w
     
     
     def iterate(self):
         for outer_loop_indx in range(self.num_outer_loop_epochs):
             for (X_tr, y_tr, X_ts, y_ts) in zip(self.X_trains, self.y_trains, 
                                                 self.X_tests, self.y_tests):
-                for _ in range(self.num_inner_loop_epochs):
-                    y_pred = X_tr @ self.w
-                    delta = (y_pred - y_tr)[:,np.newaxis]
-                    self.w = self.w - self.alpha * np.sum(delta * X_tr,0)
-
+                
+                self.inner_loop_updates(X_tr, y_tr, 
+                                        self.num_inner_loop_epochs)
                 y_pred_ts = X_ts @ self.w
                 delta = (y_pred_ts - y_ts)[:,np.newaxis]
                 if self.second_order:
                     self.w = self.w - self.alpha *np.sum((delta * X_ts) @ 
-                                                         (np.eye(2) - self.alpha * X_tr.T @ X_tr), 0)
+                                                         (np.eye(2) - 
+                                                          self.alpha * X_tr.T @ X_tr), 0)
                 else:
                     self.w = self.w - self.alpha *np.sum(delta * X_ts,0)
-                    
-                    
-
+                                        
                 delta_tr = (X_tr @ self.w - y_tr)**2
                 delta_ts = (X_ts @ self.w - y_ts)**2
 
